35cea132b9b2676614368879cfc5a1ef57c6ba69
TomaszBartecki <tommaso1795@gmail.com>
2019-09-17 15:15:40 +0200

    first commit

---
+++ SortFromLibrary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortFromLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 5, 4, 3, 2, 1, -1, 0};
+            numbers.Sort(); 
+            foreach (var i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
